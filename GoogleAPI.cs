93be8d67a9be9013cc850524385b031aaa094923
Yunnna005 <117185683+Yunnna005@users.noreply.github.com>
2025-09-04 01:27:25 +0100

    Calendar v1 (#1)
    * Created GoogleAPI class with the code from google documentation; needs to be fixed to me my requirements
    
    * Added test solution for google calendar. The design need to be changed.
    Succesufull test.

---
+++ GoogleAPI.cs
@@ -0,0 +1,50 @@
+﻿using Google.Apis.Auth.OAuth2;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Google.Apis.Util.Store;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using System.Collections.Generic;
+
+namespace MyWidgets
+{
+    internal class GoogleAPI
+    {
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "MyWidgetsApp";
+
+        public static List<Event> GetEvents()
+        {
+            UserCredential credential;
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            var request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 2500;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            Events events = request.Execute();
+            return events.Items != null ? new List<Event>(events.Items) : new List<Event>();
+        }
+    }
+    }
