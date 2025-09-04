d4023ee4e1a482838df9fb6a181082aa765cf2b1
Yunnna005 <annakovalenko0500@gmail.com>
2025-09-04 01:31:11 +0100

    Added test solution for google calendar. The design need to be changed. Succesufull test.

---
+++ frmGoogleCalendar.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MyWidgets
+{
+    partial class frmGoogleCalendar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.listBoxEvents = new System.Windows.Forms.ListBox();
+            this.btnLoadEvents = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(245, 138);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // listBoxEvents
+            // 
+            this.listBoxEvents.FormattingEnabled = true;
+            this.listBoxEvents.ItemHeight = 16;
+            this.listBoxEvents.Location = new System.Drawing.Point(92, 145);
+            this.listBoxEvents.Name = "listBoxEvents";
+            this.listBoxEvents.Size = new System.Drawing.Size(120, 84);
+            this.listBoxEvents.TabIndex = 1;
+            // 
+            // btnLoadEvents
+            // 
+            this.btnLoadEvents.Location = new System.Drawing.Point(552, 182);
+            this.btnLoadEvents.Name = "btnLoadEvents";
+            this.btnLoadEvents.Size = new System.Drawing.Size(148, 46);
+            this.btnLoadEvents.TabIndex = 2;
+            this.btnLoadEvents.Text = "Load Events";
+            this.btnLoadEvents.UseVisualStyleBackColor = true;
+            this.btnLoadEvents.Click += new System.EventHandler(this.button1btnLoadEvents_Click);
+            // 
+            // frmGoogleCalendar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoadEvents);
+            this.Controls.Add(this.listBoxEvents);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "frmGoogleCalendar";
+            this.Text = "frmGoogleCalendar";
+            this.Load += new System.EventHandler(this.frmGoogleCalendar_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ListBox listBoxEvents;
+        private System.Windows.Forms.Button btnLoadEvents;
+    }
+}
---
+++ frmGoogleCalendar.cs
@@ -0,0 +1,68 @@
+﻿using Google.Apis.Calendar.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWidgets
+{
+    public partial class frmGoogleCalendar : Form
+    {
+
+        private List<Event> allEvents = new List<Event>();
+        public frmGoogleCalendar()
+        {
+            InitializeComponent();
+        }
+
+        private void frmGoogleCalendar_Load(object sender, EventArgs e)
+        {
+            monthCalendar1.DateSelected += MonthCalendar1_DateSelected;
+        }
+
+
+        private void button1btnLoadEvents_Click(object sender, EventArgs e)
+        {
+            allEvents = GoogleAPI.GetEvents();
+            MessageBox.Show("Events loaded!");
+            HighlightEventDays();
+        }
+
+        private void MonthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            listBoxEvents.Items.Clear();
+            DateTime selectedDate = monthCalendar1.SelectionStart;
+
+            var eventsForDay = allEvents.Where(ev =>
+            {
+                DateTime? start = ev.Start.DateTime ?? DateTime.Parse(ev.Start.Date);
+                return start.Value.Date == selectedDate.Date;
+            }).ToList();
+
+            foreach (var ev in eventsForDay)
+            {
+                string time = ev.Start.DateTime?.ToString("HH:mm") ?? "All Day";
+                listBoxEvents.Items.Add($"{time} - {ev.Summary}");
+            }
+        }
+
+        private void HighlightEventDays()
+        {
+            // Optional: you can use BoldedDates to highlight days with events
+            monthCalendar1.RemoveAllBoldedDates();
+            foreach (var ev in allEvents)
+            {
+                DateTime? start = ev.Start.DateTime ?? DateTime.Parse(ev.Start.Date);
+                monthCalendar1.AddBoldedDate(start.Value.Date);
+            }
+            monthCalendar1.UpdateBoldedDates();
+        }
+
+
+    }
+}
